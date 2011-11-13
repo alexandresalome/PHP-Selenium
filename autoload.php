4f55aa60d539db9d0b48cbaf2ff469f621597957
alexandresalome <alexandre.salome@gmail.com>
2011-11-13 14:05:48 +0100

    [Project] Add autoload script

---
+++ autoload.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of PHP Selenium Library.
+ * (c) Alexandre Salomé <alexandre.salome@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once __DIR__.'/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+
+$loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
+
+$loader->registerNamespaces(array(
+    'Symfony'  => __DIR__.'/vendor',
+    'Buzz'     => __DIR__.'/vendor/buzz/lib',
+    'Selenium' => array(__DIR__.'/src', __DIR__.'/tests')
+));
+
+$loader->register();
